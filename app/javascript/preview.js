6875d2eb2a4ed24455a580b0797703e1b2fa7fc3
tamotsuokiura <umi.is.pucapuca@gmail.com>
2020-08-28 10:57:27 +0900

    Update preview.js

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -5,16 +5,19 @@ if(window.location.href.match(/\/items\/new/) || window.location.href.match(/\/i
 
     document.getElementById("item_image").addEventListener("change", function(e){
       const imageContent = document.getElementById("current-image");
+      const addImage = document.getElementById("add-image");
       if (imageContent){
         imageContent.remove();
-      };
-
+      } else if (addImage){
+        addImage.remove();
+      }
       const file = e.target.files[0];
       const imageURL = window.URL.createObjectURL(file);
 
       const imageElement = document.createElement("div");
       const blobImage= document.createElement("img");
       blobImage.setAttribute("src", imageURL);
+      blobImage.setAttribute("id", "add-image")
       
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
